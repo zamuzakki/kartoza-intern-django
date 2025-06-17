8b458f1b289e4ef84905f8338ada22caef40c81c
Z.A. Muzakki <zakki@kartoza.com>
2025-06-17 14:16:02 +0700

    Add useful index

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Staff
 
 # Create your views here.
 def index(request):
-    return HttpResponse('You are looking at the index page.')
+    staffs = Staff.objects.all().order_by('name')
+    output = ', '.join([str(staff) for staff in staffs])
+    return HttpResponse(output)
 
 
 def detail(request, staff_id):
