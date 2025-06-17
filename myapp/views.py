74e078e15d4e276885523751dcb5a12a6f8a434d
Z.A. Muzakki <zakki@kartoza.com>
2025-06-17 14:08:55 +0700

    add basic urls & view

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('You are looking at the index page.')
+
+
+def detail(request, staff_id):
+    return HttpResponse('You are looking at the detail page.')
+
+
+def create(request):
+    return HttpResponse('You are looking at the create page.')
